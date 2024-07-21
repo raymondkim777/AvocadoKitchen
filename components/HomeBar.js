05ca6db98de4f5e65596be915e99272ec3e3bfd4
raymondkim777 <raymondkim123456789@gmail.com>
2024-07-21 22:56:36 +0900

    homebar

---
+++ components/HomeBar.js
@@ -0,0 +1,33 @@
+import React, {useState,} from 'react';
+import { Text, View, SafeAreaView, Image,  TextInput, TouchableOpacity, StyleSheet, Platform, FlatList } from 'react-native';
+
+const HomeBar = () => {
+
+  const buttons = [
+    {id: 'tutorial',},
+    {id: 'browse',},
+    {id: 'add-meal',},
+    {id: 'info',},
+    {id: 'profile',},
+  ]
+
+  return (
+    <View className={` flex flex-row w-full h-20 bg-homeBgLight items-center justify-center`}>
+      {buttons.map((item, index) => (
+        index == 0? 
+          <View className='flex flex-1 h-full ml-2 items-center justify-center'>
+            <TouchableOpacity key={item.id} className={`w-16 h-16 rounded-full bg-homeBgDark`}/>
+          </View>
+        : index == 4? 
+          <View className='flex flex-1 h-full mr-2 items-center justify-center'>
+            <TouchableOpacity key={item.id} className={`w-16 h-16 rounded-full bg-homeBgDark`}/>
+          </View>
+        : <View className='flex flex-1 h-full items-center justify-center'>
+            <TouchableOpacity key={item.id} className={`w-16 h-16 rounded-full bg-homeBgDark`}/>
+          </View>
+      ))}
+    </View>
+  )
+}
+
+export default HomeBar;
