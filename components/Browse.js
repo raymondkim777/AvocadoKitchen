6cce460261ad8cc0d73ca55b593e78a486ac0189
raymondkim777 <raymondkim123456789@gmail.com>
2024-07-24 19:47:56 +0900

    options

--- components/Browse.js
+++ components/Browse.js
@@ -1,7 +1,13 @@
 import React, {useState,} from 'react';
-import { Text, View, SafeAreaView, Image,  TextInput, TouchableOpacity, StyleSheet, Platform, ScrollView, FlatList } from 'react-native';
+import { Text, View, SafeAreaView, Image, Dimensions, TextInput, TouchableOpacity, StyleSheet, Platform, ScrollView, FlatList } from 'react-native';
 
-const OptionButton = ({title, index}) => (
+const OptionButton = ({callback}) => (
+  <TouchableOpacity activeOpacity={0.5} className='w-8 h-8 ml-2 bg-buttonBg rounded-lg'>
+
+  </TouchableOpacity>
+)
+
+const DietButton = ({title, index}) => (
   <View className='flex-row w-fit h-7 items-center justify-center'>
     <TouchableOpacity activeOpacity={1} className='w-4 h-4 rounded-md border-2 border-itemText mr-2'
     />
@@ -11,19 +17,27 @@ const OptionButton = ({title, index}) => (
   </View>
 )
 
+const { width, height } = Dimensions.get('window');
+
 const Browse = () => {
   
-  { /* State/Functions */}
-  const [searchQuery, setSearchQuery] = useState('');
-
   {/* Data */}
   const optionList = [
     'Lacto-Ovo', 
     'Lacto', 
     'Ovo', 
-    'Vegan', 
+    'Vegan',
+    'Vegetarian', 
     'Pescastarian',
   ]
+
+  const categories = [
+    'All', 'Name', 'User', 'Tag',
+  ]
+
+  { /* State/Functions */}
+  const [searchQuery, setSearchQuery] = useState('');
+
   const [options, setOptions] = useState(new Array(optionList.length).fill(false))
   const updateOptions = (index) => {
     const new_options = options;
@@ -31,10 +45,24 @@ const Browse = () => {
     setOptions(new_options);
   }
 
+  const [searchCat, setSearchCat] = useState(0);
+  const [catColor, setCatColor] = useState(['bg-itemText', '', '', '']);
+  const [catText, setCatText] = useState(['text-itemBgLight', 'text-itemText', 'text-itemText', 'text-itemText']);
+  const setCatFocus = (index) => {
+    setSearchCat(index);
+    const new_color = new Array(categories.length).fill('');
+    new_color[index] = 'bg-itemText';
+    setCatColor(new_color);
+
+    const new_text = new Array(categories.length).fill('text-itemText');
+    new_text[index] = 'text-itemBgLight';
+    setCatText(new_text);
+  }
+
   return (
-    <SafeAreaView id='screen' className='flex flex-col w-full h-full justify-center items-center'>
-      <ScrollView id='scroll' className='w-full h-full'>
-        <View id='content' className='w-full h-fit p-4 bg-screenBg'>
+    <SafeAreaView id='screen' className='w-full h-full justify-center items-center'>
+      <ScrollView id='scroll' className='grow w-full h-full bg-red-100'>
+        <View id='content' className='grow w-full h-fit p-4 bg-screenBg'>
           {/* Frame 1 - Search Bar */}
           <View className='w-full h-fit mt-2'>
             <Text className="font-inconsolata mx-4 text-3xl text-screenText">
@@ -47,19 +75,18 @@ const Browse = () => {
                 onChangeText={setSearchQuery} 
                 underlineColorAndroid={'transparent'}
               />
-              <TouchableOpacity activeOpacity={0.5} className='w-8 h-8 ml-2 bg-buttonBg rounded-lg'>
+              <OptionButton/>
 
-              </TouchableOpacity>
             </View>
           </View>
 
           {/* Frame 2 - Options */}
           <View className='w-full h-fit mt-6'>
             {/* Checkbox */}
-            <View className='w-full h-16 items-center justify-center px-2 bg-buttonBg rounded-lg'>
+            <View className='w-full min-h-16 h-fit items-center justify-center px-2 bg-buttonBg rounded-lg'>
               <View className='flex-row flex-wrap w-full h-fit'>
                 {optionList.map((option_name, index) => (
-                  <OptionButton title={option_name} index={index}/>
+                  <DietButton title={option_name} index={index}/>
                 ))}
               </View>
             </View>
@@ -70,7 +97,32 @@ const Browse = () => {
             </View>
 
             {/* More Search Options */}
-            <View className='w-full h-8 mt-2 bg-buttonBg rounded-lg'>
+            <View className='flex-row w-full h-8 mt-2 items-center justify-center px-3 bg-buttonBg rounded-lg'>
+              <Text className='font-inconsolata text-base'>
+                Search By:
+              </Text>
+              <View className='flex-row grow w-fit h-fit items-center justify-center'>
+                {categories.map((cat, index) => (
+                  <TouchableOpacity className={`w-12 h-6 items-center justify-center rounded-full ${catColor[index]}`}
+                    activeOpacity={1} onPress={()=>setCatFocus(index)}>
+                    <Text className={`font-inconsolata text-base ${catText[index]}`}>{cat}</Text>
+                  </TouchableOpacity>
+                ))}
+                <View id='div' className="{{height > 800 ? 'w-4': ''}}"/>
+              </View>
+            </View>
+          </View>
+
+          {/* Frame 3 - Results */}
+          <View className='flex-col w-full h-fit mt-6 bg-blue-100'>
+            <View className='flex-row w-full h-fit justify-between bg-red-100'>
+              <Text className='font-inconsolata text-xl ml-4 text-itemBgLight'>
+                Results
+              </Text>
+              <OptionButton/>
+            </View>
+            <View className='w-full h-20'>
+
             </View>
           </View>
         </View>
