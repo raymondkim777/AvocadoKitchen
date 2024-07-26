74f1ac320039458d39fa0a8a7a662c8f13942c13
raymondkim777 <raymondkim123456789@gmail.com>
2024-07-26 23:07:15 +0900

    procedure empty

--- components/RecipePage.js
+++ components/RecipePage.js
@@ -36,7 +36,6 @@ const ProcedureDiv = ({width}) => (
   <View className={`w-${width} h-full`}/>
 )
 
-
 const { width, height } = Dimensions.get('window');
 
 const RecipePage = ({
@@ -160,31 +159,37 @@ const RecipePage = ({
               </View>
             </View>
           </View>
-
+          
           {/* Procedure */}
-          <View className='grow flex-col w-full h-fit items-center justify-center mt-2'>
-            <View className='flex-row w-full h-10 items-center'>
-              <Text className='font-inconsolata mx-4 text-screenText text-xl'>
-                Procedure
-              </Text>
-            </View>
+          {
+            procedure.length == 0
+            ? null
+            : (
+              <View className='grow flex-col w-full h-fit items-center justify-center mt-2'>
+                <View className='flex-row w-full h-10 items-center'>
+                  <Text className='font-inconsolata mx-4 text-screenText text-xl'>
+                    Procedure
+                  </Text>
+                </View>
 
-            {/* Procedure Cards */}
-            <View onLayout={onLayout} className='grow w-full h-fit rounded-xl overflow-hidden'>
-              <FlatList className='w-full h-80'
-                horizontal={true}
-                showsHorizontalScrollIndicator={false}
-                pagingEnabled
-                snapToInterval={viewWidth + 14}
-                snapToAlignment='start'
-                decelerationRate='fast'
-                data={procedure}
-                renderItem={({item}) => <ProcedureCard item={item} width={viewWidth}/>}
-                ItemSeparatorComponent={<ProcedureDiv width={4}/>}
-                keyExtractor={item => item.id}
-                />
-            </View>
-          </View>
+                {/* Procedure Cards */}
+                <View onLayout={onLayout} className='grow w-full h-fit rounded-xl overflow-hidden'>
+                  <FlatList className='w-full h-80'
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    pagingEnabled
+                    snapToInterval={viewWidth + 14}
+                    snapToAlignment='start'
+                    decelerationRate='fast'
+                    data={procedure}
+                    renderItem={({item}) => <ProcedureCard item={item} width={viewWidth}/>}
+                    ItemSeparatorComponent={<ProcedureDiv width={4}/>}
+                    keyExtractor={item => item.id}
+                    />
+                </View>
+              </View>
+            )
+          }
 
           {/* Buttons */}
           <View className='flex-row w-full h-fit items-center justify-between mt-8 mb-4 px-6'>
