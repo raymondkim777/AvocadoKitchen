57db20e4d3323c1682aa1f672809b19beded8e4b
raymondkim777 <raymondkim123456789@gmail.com>
2024-08-14 16:06:55 +0900

    color

--- components/general/SideBarButton.js
+++ components/general/SideBarButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { TouchableOpacity } from 'react-native';
 
 const SideBarButton = ({ callback }) => (
-  <TouchableOpacity className='w-8 h-8 bg-red-100 rounded-lg'
+  <TouchableOpacity className='w-8 h-8 bg-buttonBg rounded-lg'
   activeOpacity={0.9} onPress={()=>callback(true)}>
 
   </TouchableOpacity>
