b19bea78930d6e3fcbd495535da9e716345637c8
Soree <kimsoree925@gmail.com>
2024-08-23 08:26:27 +0900

    page popup update

--- components/general/SideBar.js
+++ components/general/SideBar.js
@@ -1,31 +1,31 @@
-import React, { useState } from 'react';
-import { CommonActions } from '@react-navigation/native';
+import React, { useState, } from 'react';
+import { View, Image, TouchableOpacity, Modal, } from 'react-native';
+import TitleTextComponent from '../text/TitleTextComponent';
+import SideBarPage from './SideBarPage';
 import SideBarModal from './SideBarModal';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import SideBarView from './SideBarView';
 
-const SideBar = ({ 
-  navigation, 
-  wideScreen, username, 
-  showSideBar, setShowSideBar,
-  pages, buttonCSS, textCSS, updatePage,
- }) => (
-  wideScreen
-  ? <SideBarView
-    pages={pages}
-    username={username}
-    updatePage={updatePage}
-    buttonCSS={buttonCSS}
-    textCSS={textCSS} 
-    />
-  : <SideBarModal 
-    pages={pages}
-    username={username}
-    showSideBar={showSideBar}
-    setShowSideBar={setShowSideBar}
-    updatePage={updatePage}
-    buttonCSS={buttonCSS}
-    textCSS={textCSS}
-    />
-)
+const SideBar = ({ wideScreen, username, showSideBar, setShowSideBar,pages, updatePage, buttonCSS, textCSS}) => {
+  return (
+    wideScreen
+    ? <SideBarView
+      pages={pages}
+      username={username}
+      updatePage={updatePage}
+      buttonCSS={buttonCSS}
+      textCSS={textCSS} 
+      />
+    : <SideBarModal 
+      pages={pages}
+      username={username}
+      showSideBar={showSideBar}
+      setShowSideBar={setShowSideBar}
+      updatePage={updatePage}
+      buttonCSS={buttonCSS}
+      textCSS={textCSS}
+      />
+  )
+}
 
 export default SideBar
--- components/pages/HomeControl.js
+++ components/pages/HomeControl.js
@@ -1,38 +1,22 @@
-import React, {useState, useTransition, createContext} from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { CommonActions } from '@react-navigation/native';
+import React, {useState, useTransition,createContext} from 'react';
 import { Text, View, Dimensions, SafeAreaView, Image,ScrollView,  TextInput, TouchableOpacity, StyleSheet, Platform, FlatList } from 'react-native';
 import 'intl-pluralrules';
 import '../text/i18n'
-import HomePage from './HomePage';
-import MyMeals from './MyMeals';
-import Browse from './Browse';
-import RecipePage from './RecipePage';
-import AddMealPage from './AddMealPage';
-import AddMealPage2 from './AddMealPage2';
-import ProfilePage from './ProfilePage';
-import UserInfoPage from './UserInfoPage';
-import Tutorial from './Tutorial';
 import SideBar from '../general/SideBar';
 import PagePopup from '../general/PagePopup';
 
 const { width, height } = Dimensions.get('window');
-const PageStack = createStackNavigator();
 export const SideBarContext = createContext();
 
 const HomeControl = ({ navigation }) => {
-  {/* SideBar Context */}
+  {/* SideBar */}
   const [showSideBar, setShowSideBar] = useState(false);
-  const pageID = [
-    'HomePage', 
-    'MyMeals',
-    'Browse',
-    'AddMealPage',
-    'UserInfoPage',
-    'ProfilePage',
-    'Tutorial',
-  ];
-  const pages = [
+  
+  const [username, setUsername] = useState('Username');
+  
+  const [showScreenIdx,setScreenIdx] = useState(0);
+
+    const pages = [
     'Home', 
     'My Meals',
     'Recipe Search', 
@@ -67,61 +51,39 @@ const HomeControl = ({ navigation }) => {
     const new_text = new Array(pages.length).fill('text-itemText');
     new_text[index] = 'text-itemBgLight';
     setTextCSS(new_text);
-
-
+    setScreenIdx(index);
   }
+
   
-  {/* HomeControl */}
-  const [username, setUsername] = useState('Username');
-  
+
   
   const wideScreen = Platform.OS === 'ios' ? (height / width) < 1.6: (height / width) < 1.4;
   const Container = wideScreen ?  View : SafeAreaView;
-
   const SideBarContextValue = {wideScreen, setShowSideBar, updatePage};
   return (
     <Container className='flex flex-row w-full h-full justify-center items-center bg-screenBg'>
       {/* SideBar */}
-      <SideBar 
-      navigation={navigation}
-      PageStack={PageStack}
-      wideScreen={wideScreen}
-      username={username}
-      showSideBar={showSideBar}
-      setShowSideBar={setShowSideBar}
-      pages={pages}
-      buttonCSS={buttonCSS}
-      textCSS={textCSS} 
-      updatePage={updatePage}
-      />
-       {/*
       <SideBarContext.Provider value={SideBarContextValue}>
-        <PageStack.Navigator 
-          initialRouteName="HomePage"
-          screenOptions={{ headerShown: false, animationEnabled: false }}
-         
-        >
-          <PageStack.Screen name="HomePage" component={HomePage}/>
-          <PageStack.Screen name="MyMeals" component={MyMeals} />
-          <PageStack.Screen name="Browse" component={Browse} />
-          <PageStack.Screen name="AddMealPage" component={AddMealPage} />
-          <PageStack.Screen name="AddMealPage2" component={AddMealPage2} />
-          <PageStack.Screen name="UserInfoPage" component={UserInfoPage} />
-          <PageStack.Screen name="ProfilePage" component={ProfilePage} />
-          <PageStack.Screen name="Tutorial" component={Tutorial} />
-        </PageStack.Navigator>        
-      </SideBarContext.Provider>
-       */}
-
-      <SideBarContext.Provider value={SideBarContextValue}>
+        <SideBar 
+        wideScreen={wideScreen}
+        username={username}
+        showSideBar={showSideBar}
+        setShowSideBar={setShowSideBar}
+        setScreenIdx = {setScreenIdx}
+        buttonCSS = {buttonCSS}
+        textCSS = {textCSS}
+        pages={pages}
+        updatePage={updatePage}
+        />
+        
         <PagePopup 
         navigation={navigation}
         wideScreen={wideScreen}
-        index={pageIndex} 
-        setShowSideBar={setPageIndex}/>
-       </SideBarContext.Provider>
+        index={showScreenIdx} 
+        setShowSideBar={setShowSideBar}
+        />
+      </SideBarContext.Provider>
      
-        
     </Container>
   );
 }
