b895024de43933bd3e819f0accf4748cb38e3b4f
raymondkim777 <raymondkim123456789@gmail.com>
2024-08-25 22:25:40 +0900

    icons

--- components/general/ExitButton.js
+++ components/general/ExitButton.js
@@ -2,6 +2,7 @@ import React, { useState, useContext, } from 'react';
 import { SideBarContext } from '../pages/HomeControl';
 import { View, TouchableOpacity } from 'react-native';
 import LeaveAlert from './LeaveAlert';
+import Exit from "../../assets/icons/exit.svg";
 
 const ExitButton = ({ exitCheck = false }) => {
   const {wideScreen, setShowSideBar, updatePage} = useContext(SideBarContext);
@@ -17,9 +18,9 @@ const ExitButton = ({ exitCheck = false }) => {
   return(
     <View className='w-fit h-fit'>
       <LeaveAlert showAlert={showAlert} setShowAlert={setShowAlert} />
-      <TouchableOpacity className='w-8 h-8 bg-buttonBg rounded-lg'
+      <TouchableOpacity className='w-8 h-8 bg-buttonBg rounded-lg items-center justify-center'
       activeOpacity={0.9} onPress={()=>(handleExit(exitCheck))}>
-
+        <Exit width={30} height={30} stroke={'#85855B'} strokeWidth={3} />
       </TouchableOpacity>
     </View>
   )
--- components/general/SideBarButton.js
+++ components/general/SideBarButton.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
-import Logo from "../../assets/icons/play.svg";
+import RightArrow from '../../assets/icons/rightarrow.svg';
 
 const SideBarButton = ({ callback }) => (
-  <TouchableOpacity className='w-8 h-8 bg-buttonBg rounded-lg'
+  <TouchableOpacity className='w-8 h-8 bg-buttonBg rounded-lg items-center justify-center'
   activeOpacity={0.9} onPress={()=>callback(true)}>
-    <Logo width={30} height={30} fill={'#90BCAF'} stroke={'#85855B'} strokeWidth={1} />
+    <RightArrow width={30} height={30} stroke={'#85855B'} strokeWidth={3} />
   </TouchableOpacity>
 )
 
