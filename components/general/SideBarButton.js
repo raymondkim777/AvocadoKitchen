41d51840e0c815231eca086df08ee1442bbcd86c
Soree <kimsoree925@gmail.com>
2024-08-25 15:55:55 +0900

    svg update

--- components/general/SideBarButton.js
+++ components/general/SideBarButton.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
+import Logo from "../../assets/icons/play.svg";
 
 const SideBarButton = ({ callback }) => (
   <TouchableOpacity className='w-8 h-8 bg-buttonBg rounded-lg'
   activeOpacity={0.9} onPress={()=>callback(true)}>
-
+    <Logo width={30} height={30} fill={'#90BCAF'} stroke={'#85855B'} strokeWidth={1} />
   </TouchableOpacity>
 )
 
