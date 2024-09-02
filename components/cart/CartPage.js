3301759032a3055ec86581cf7f38191bafece8c9
raymondkim777 <raymondkim123456789@gmail.com>
2024-09-02 22:08:57 +0900

    separate counts

--- components/cart/CartPage.js
+++ components/cart/CartPage.js
@@ -6,6 +6,7 @@ import ItemTextComponent from '../text/ItemTextComponent';
 import ItemLargeTextComponent from '../text/ItemLargeTextComponent';
 import SideBarButton from '../general/SideBarButton';
 import ExitButton from '../general/ExitButton';
+import SmallButton from '../general/SmallButton';
 import EditButton from '../general/EditButton';
 import CartEditModal from './CartEditModal';
 
@@ -67,13 +68,43 @@ const ItemCard = ({
   )
 }
 
+const SiteSummary = ({siteIdx, data}) => {
+  let totalPrice = 0;
+  for (let i = 0; i < data.length; i++) {
+    totalPrice += data[i].price * data[i].quantity;
+  }
+  return(
+    <View className='flex-row w-full h-fit mt-4 justify-end'>
+      <View className='flex-row w-fit h-full mr-4'>
+        <TitleTextComponent translate={true} size={'text-xl'} css={'text-itemText'}>
+          Total
+        </TitleTextComponent>
+        <TitleTextComponent size={'text-xl'} css={'text-itemText'}>
+          :
+        </TitleTextComponent>
+        <TitleTextComponent size={'text-xl'} css={'ml-2 text-itemText'}>
+          {totalPrice}
+        </TitleTextComponent>
+        <TitleTextComponent size={'text-xl'} css={'text-itemText'}>
+          원
+        </TitleTextComponent>
+      </View>
+    </View>
+  )
+}
+
 const CartPage = ({ viewWidth }) => {
   const {wideScreen, setShowSideBar, updatePage} = useContext(SideBarContext);
 
   const [showEditModal, setShowEditModal] = useState(false);
   const [siteIndex, setSiteIndex] = useState(0);
   const [itemIndex, setItemIndex] = useState(0);
-  const handleUpdateEdit = () => {
+
+  const handleResetCart = () => {
+    null;
+  }
+
+  const handleAddIngredient = () => {
     null;
   }
 
@@ -86,7 +117,7 @@ const CartPage = ({ viewWidth }) => {
           id: 1, 
           title: '복숭아',
           fastDelivery: true,
-          price: '15900',
+          price: 15900,
           quantity: 2,
           image: require('../../assets/images/info-example/tofu.jpg'),
         },
@@ -94,15 +125,15 @@ const CartPage = ({ viewWidth }) => {
           id: 2, 
           title: '아삭 복숭아 1.2kg [품종:오도로끼-(딱복)]',
           fastDelivery: true,
-          price: '15900',
-          quantity: 2,
+          price: 15900,
+          quantity: 3,
           image: require('../../assets/images/info-example/tofu.jpg'),
         },
         {
           id: 3, 
           title: '아삭 복숭아 1.2kg [품종:오도로끼-(딱복)]',
           fastDelivery: false,
-          price: '15900',
+          price: 15900,
           quantity: 2,
           image: require('../../assets/images/info-example/tofu.jpg'),
         },
@@ -110,8 +141,8 @@ const CartPage = ({ viewWidth }) => {
           id: 4, 
           title: '아삭 복숭아 1.2kg [품종:오도로끼-(딱복)]',
           fastDelivery: false,
-          price: '15900',
-          quantity: 2,
+          price: 15900,
+          quantity: 3,
           image: require('../../assets/images/info-example/tofu.jpg'),
         },
       ],
@@ -124,7 +155,7 @@ const CartPage = ({ viewWidth }) => {
           id: 5, 
           title: '아삭 복숭아 1.2kg [품종:오도로끼-(딱복)]',
           fastDelivery: true,
-          price: '15900',
+          price: 15900,
           quantity: 2,
           image: require('../../assets/images/info-example/tofu.jpg'),
         },
@@ -132,7 +163,7 @@ const CartPage = ({ viewWidth }) => {
           id: 6, 
           title: '아삭 복숭아 1.2kg [품종:오도로끼-(딱복)]',
           fastDelivery: true,
-          price: '15900',
+          price: 15900,
           quantity: 2,
           image: require('../../assets/images/info-example/tofu.jpg'),
         },
@@ -140,7 +171,7 @@ const CartPage = ({ viewWidth }) => {
           id: 7, 
           title: '아삭 복숭아 1.2kg [품종:오도로끼-(딱복)]',
           fastDelivery: false,
-          price: '15900',
+          price: 15900,
           quantity: 2,
           image: require('../../assets/images/info-example/tofu.jpg'),
         },
@@ -148,8 +179,8 @@ const CartPage = ({ viewWidth }) => {
           id: 8, 
           title: '아삭 복숭아 1.2kg [품종:오도로끼-(딱복)]',
           fastDelivery: false,
-          price: '15900',
-          quantity: 2,
+          price: 15900,
+          quantity: 3,
           image: require('../../assets/images/info-example/tofu.jpg'),
         },
       ],
@@ -169,8 +200,17 @@ const CartPage = ({ viewWidth }) => {
         <ExitButton />
       </View>
 
+      <View className='flex-row w-full h-fit items-center mt-6'>
+        <View className='w-fit h-fit'>
+          <SmallButton text='Reset Cart' callback={handleResetCart}/>
+        </View>
+        <View className='w-fit h-fit ml-2'>
+          <SmallButton text='Add Ingredient' callback={handleAddIngredient}/>
+        </View>
+      </View>
+
       {/* SectionList */}
-      <View className='shrink w-full h-full mt-6 rounded-xl overflow-hidden'>
+      <View className='shrink w-full h-full mt-4 rounded-xl overflow-hidden'>
         <SectionList
         className='w-full h-full px-2 pr-1 bg-itemBgLight'
         scrollEnabled={true}
@@ -199,14 +239,16 @@ const CartPage = ({ viewWidth }) => {
             </TitleTextComponent>
           </View>
         )}
+        renderSectionFooter={({section: {siteIdx, data}}) => (
+          <SiteSummary siteIdx={siteIdx} data={data} />
+        )}
         ListHeaderComponent={
         <CartEditModal 
         item={items[siteIndex].data[itemIndex]}
         siteIndex={siteIndex}
         viewWidth={viewWidth}
         showEditModal={showEditModal} 
-        setShowEditModal={setShowEditModal} 
-        handleUpdateEdit={handleUpdateEdit} />
+        setShowEditModal={setShowEditModal} />
       }
         ListFooterComponent={<View className='w-full h-2' />}
         ItemSeparatorComponent={ItemDiv}
