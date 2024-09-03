b680ea035993b9db83667668bac6144383d0fc67
raymondkim777 <raymondkim123456789@gmail.com>
2024-09-03 21:27:37 +0900

    disable feature

--- components/general/SmallButton.js
+++ components/general/SmallButton.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { View, TouchableHighlight } from 'react-native';
 import TitleTextComponent from '../text/TitleTextComponent';
 
-const SmallButton = ({text, callback}) => (
-  <TouchableHighlight className='w-fit h-9 rounded-lg'
-  activeOpacity={0.9} onPress={callback}>
+const SmallButton = ({text, callback, disabled=false}) => (
+  <TouchableHighlight className={`w-fit h-9 rounded-lg`}
+  activeOpacity={0.9} onPress={callback} disabled={disabled}>
     <View className='w-fit h-full px-3 items-center justify-center bg-buttonBg rounded-lg'>
-      <TitleTextComponent translate={true} bold={true} size={'text-xl'} css={'text-itemText'}>
+      <TitleTextComponent translate={true} bold={true} size={'text-xl'} css={disabled ? 'text-grayText' : 'text-itemText'}>
         {text}
       </TitleTextComponent>
     </View>
