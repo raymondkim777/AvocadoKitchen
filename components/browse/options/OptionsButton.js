9e4acb1f85035f251603499ec52fe5139f6c46c8
Raymond Kim :D <raymondkim123456789@gmail.com>
2024-09-21 21:46:15 -0500

    increased optionsbutton size to fit other button sizes

--- components/browse/options/OptionsButton.js
+++ components/browse/options/OptionsButton.js
@@ -3,10 +3,10 @@ import { TouchableHighlight, View } from 'react-native';
 import List from "../../../assets/icons/list.svg";
 
 const OptionsButton = ({callback, showOptions}) => (
-  <TouchableHighlight className='w-8 h-8 ml-3 rounded-lg'
+  <TouchableHighlight className='w-9 h-9 ml-3 rounded-lg'
   activeOpacity={0.9} onPress={()=>callback(!showOptions)}>
     <View className='w-full h-full items-center justify-center rounded-lg bg-buttonBg'>
-      <List width={25} height={25} stroke={'#85855B'} strokeWidth={3} />
+      <List width={28} height={28} stroke={'#85855B'} strokeWidth={3} />
     </View>
   </TouchableHighlight>
 )
